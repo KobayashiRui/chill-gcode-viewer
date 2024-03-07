ae517093f4c9e9ed76ab0597526b797be7e3913a
KobayashiRui <roboroborx782@gmail.com>
2024-03-08 07:29:10 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,54 @@
-import { useState } from "react";
+import {useEffect, useState, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import GcodeEditor from "./components/GcodeEditor";
+import GcodeViewer from "./components/GcodeViewer";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const editorRef = useRef<any>(null);
+  const viewerRef = useRef<any>(null);
+  const [parentHeight, setParentHeight] = useState(0);
+  const [viewerWidth, setViewerWidth] = useState(0);
+  const [editorWidth, setEditorWidth] = useState(0);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  useEffect(() => {
+    function handleResize() {
+     if(editorRef.current) {
+        setParentHeight(editorRef.current.clientHeight-20); // 親要素の現在の高さをセット
+        setEditorWidth(window.innerWidth * 2.0/6.0 - 10)
+        setViewerWidth(window.innerWidth * 4.0/6.0 - 10)
+        console.log(window.innerWidth * 4.0/6.0)
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // 初期サイズを設定
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // 空の依存配列でマウントとアンマウント時のみ実行
+
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-svh w-svw flex flex-col">
+      <h1 className="text-sky-500 text-2xl m-3">Chill Gcode Viewer</h1>
+      <div className="flex h-full w-full">
+        <div ref={editorRef} className="w-2/6 border p-1 rounded-lg h-full">
+         {
+          <GcodeEditor height={parentHeight} width={editorWidth}></GcodeEditor>
+         }
+        </div>
+        <div ref={viewerRef} className="flex-1 border p-1 rounded-lg h-full">
+          {
+            <GcodeViewer height={parentHeight} width={viewerWidth}></GcodeViewer>
+          }
+        </div>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
