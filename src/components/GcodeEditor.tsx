15450b51a2661a81c1d6acd31ba4520121b59d8a
KobayashiRui <roboroborx782@gmail.com>
2024-03-25 01:30:32 +0900

    Update GcodeEditor.tsx

--- src/components/GcodeEditor.tsx
+++ src/components/GcodeEditor.tsx
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import CodeMirror from '@uiw/react-codemirror';
+import React, {useState, useEffect, useRef} from 'react';
+import CodeMirror, {ReactCodeMirrorRef} from '@uiw/react-codemirror';
 //import { javascript } from '@codemirror/lang-javascript';
 import { useRecoilState } from 'recoil';
 import { gcodeState } from '../atoms/GcodeState';
 
 function GcodeEditor({height, width}:any) {
+  const editorRef = useRef<any>(null)
 
   //const [value, setValue] = useState(gcode_data);
   const [value, setValue] = useRecoilState(gcodeState)
@@ -12,8 +13,17 @@ function GcodeEditor({height, width}:any) {
 
   const onChange = React.useCallback((val, viewUpdate) => {
     //console.log('val:', val);
+    console.log(editorRef.current)
     setValue(val);
   }, []);
-  return <CodeMirror height={`${height}px`} width={`${width}px`} value={value} onChange={onChange} />;
+
+  useEffect(() => {
+    const editor = editorRef.current?.editor;
+    if (editor) {
+    }
+  }, [editorRef]);
+
+
+  return <CodeMirror ref={editorRef} height={`${height}px`} width={`${width}px`} value={value} onChange={onChange} />;
 }
 export default GcodeEditor;
