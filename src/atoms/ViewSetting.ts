23f98bcf8cf5b3570ecc704bb740b2688b99f13b
KobayashiRui <roboroborx782@gmail.com>
2024-08-15 02:08:29 +0900

    Update ViewSetting.ts

--- src/atoms/ViewSetting.ts
+++ src/atoms/ViewSetting.ts
@@ -4,3 +4,8 @@ export const viewSettingState = atom<any>({
   key: 'viewSettingState',
   default: {move_line: true, extrude_line: true}
 })
+
+export const viewCameraSettingState = atom<any>({
+  key: 'viewCameraSettingState',
+  default: false
+})
